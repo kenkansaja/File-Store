c07b230b235b4a903c594a3c8b133999b42f5fdf
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-20 01:00:26 +0700

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -35,7 +35,7 @@ async def help_cb(c, m):
     ]
 
     # editing as help message
-    await m.message(
+    await m.message.edit: (
         text=help_text,
         reply_markup=InlineKeyboardMarkup(buttons))
 
