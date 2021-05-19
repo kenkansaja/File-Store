f6ae40708968fecb3d0f27bb8fbbf3094310dadc
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-20 02:47:01 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,7 +57,6 @@ Saya adalah bot pembuat link permanen dari berkas yang kamu kirim 📂.
             channel = await c.get_chat(int(chat_id))
             caption += "\n\n\n**--Uploader Details:--**\n\n"
             caption += f"__📢 Channel Name:__ `{channel.title}`\n\n"
-            caption += f"__🗣 User Name:__ @{channel.username}\n\n" if channel.username else ""
             caption += f"__👤 Channel Id:__ `{channel.id}`\n\n"
             caption += f"__💬 DC ID:__ {channel.dc_id}\n\n" if channel.dc_id else ""
             caption += f"__👁 Members Count:__ {channel.members_count}\n\n" if channel.members_count else ""
@@ -65,10 +64,6 @@ Saya adalah bot pembuat link permanen dari berkas yang kamu kirim 📂.
         else: #if file not from channel
             user = await c.get_users(int(chat_id))
             caption += "\n\n\n**--Uploader Details:--**\n\n"
-            caption += f"__🦚 First Name:__ `{user.first_name}`\n\n"
-            caption += f"__🐧 Last Name:__ `{user.last_name}`\n\n" if user.last_name else ""
-            caption += f"__👁 User Name:__ @{user.username}\n\n" if user.username else ""
-            caption += f"__👤 User Id:__ `{user.id}`\n\n"
             caption += f"__💬 DC ID:__ {user.dc_id}\n\n" if user.dc_id else ""
 
         await msg.copy(m.from_user.id, caption=caption)
@@ -86,14 +81,11 @@ Saya adalah bot pembuat link permanen dari berkas yang kamu kirim 📂.
 async def me(c, m):
     me = await c.get_users(m.from_user.id)
     text = "--**YOUR DETAILS:**--\n\n\n"
-    text += f"__🦚 First Name:__ `{me.first_name}`\n\n"
-    text += f"__🐧 Last Name:__ `{me.last_name}`\n\n" if me.last_name else ""
-    text += f"__👁 User Name:__ @{me.username}\n\n" if me.username else ""
-    text += f"__👤 User Id:__ `{me.id}`\n\n"
     text += f"__💬 DC ID:__ {me.dc_id}\n\n" if me.dc_id else ""
     text += f"__✔ Is Verified By TELEGRAM:__ `{me.is_verified}`\n\n" if me.is_verified else ""
     text += f"__👺 Is Fake:__ {me.is_fake}\n\n" if me.is_fake else ""
     text += f"__💨 Is Scam:__ {me.is_scam}\n\n" if me.is_scam else ""
     text += f"__📃 Language Code:__ {me.language_code}\n\n" if me.language_code else ""
 
-    await m.reply_text(text, quote=False)
+    await m.reply_text(text, quote=True
+)
