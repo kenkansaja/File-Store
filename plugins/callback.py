d41db56783a0bbd1c07956ec249522c377bff9ce
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-20 01:03:39 +0700

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -34,12 +34,6 @@ async def help_cb(c, m):
         ]
     ]
 
-    # editing as help message
-    await m.message.edit: (
-        text=help_text,
-        reply_markup=InlineKeyboardMarkup(buttons))
-
-
 @Client.on_callback_query('close')
 async def close_cb(c, m):
     await m.message.delete()
