9c7607dac22837a3b2723220f5433b7d5ea0272b
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-05-21 17:03:41 +0700

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -39,7 +39,6 @@ async def storefile(c, m):
     # creating urls
     bot = await c.get_me()
     url = f"https://t.me/{bot.username}?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://t.me/{bot.username}?start={m.chat.id}_{msg.message_id}"
-    txt = text.replace(' ', '%20').replace('\n', '%0A').replace('--', '')
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
     # making buttons
